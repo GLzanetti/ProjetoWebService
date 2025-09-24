96c03113af8298306278f5a281f7be9e21b1a1bd
Gabriel Zanetti <gabrielzanettigo@gmail.com>
2025-09-24 20:53:12 -0300

    Primeiros passos no index.php e da classe UsersService.php

--- ApiGolSolidario/index.php
+++ ApiGolSolidario/index.php
@@ -1,3 +1,34 @@
 <?php
-    echo @$_GET['url'] ;
+
+    require_once 'service/UsersService.php';
+
+    //If para a validação da URL
+    if(@$_GET['url']){
+        $url = explode('/', @$_GET['url']);
+
+        if($url[0] == "users"){
+            
+            $service = ucfirst($url[0])."Service";
+            $method = $_SERVER['REQUEST_METHOD'];
+
+            array_shift($url);
+
+            try{
+                $response = call_user_func(array(new $service, $method), $url);
+                http_response_code(200);
+                echo ($response);
+            } catch (Exception $e) {
+                http_response_code(500);
+                echo ("Erro 500: ");
+            }
+
+        } else {
+            http_response_code(404);
+            echo ("Rota não encontrada 404");
+        }
+
+    } else {
+        http_response_code(404);
+        echo ("Rota não encontrada 404");
+    }
 ?>
